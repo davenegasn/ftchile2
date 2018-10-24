7a32e9d035e8216da6684dfc88c48863284e07c7
davenegasn <davenegasn@gmail.com>
2018-10-23 23:22:47 -0300

    Proformas edit

--- app/models/proforma.rb
+++ app/models/proforma.rb
@@ -1,6 +1,6 @@
 class Proforma < ApplicationRecord
 	has_many :article_proformas
 	has_many :articles, :through => :article_proformas
-	belongs_to :supplier, class_name: "Supplier", foreign_key: "supplier_id"
-	belongs_to :cliente, class_name: "Cliente", foreign_key: "clientes_id"
+	has_one :supplier, class_name: "Supplier", foreign_key: "supplier_id"
+	has_one :cliente, class_name: "Cliente", foreign_key: "clientes_id"
 end
