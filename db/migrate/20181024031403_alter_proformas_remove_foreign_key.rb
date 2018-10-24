ee06d0811697b95450bfd2d3121c3e297bdbadad
davenegasn <davenegasn@gmail.com>
2018-10-24 00:31:14 -0300

    Proformas edit

--- db/migrate/20181024031403_alter_proformas_remove_foreign_key.rb
+++
@@ -1,6 +0,0 @@
-class AlterProformasRemoveForeignKey < ActiveRecord::Migration[5.0]
-  def change
-  	remove_column :proformas, :clientes_id, foreign_key: true
-  	remove_column :proformas, :supplier_id, foreign_key: true
-  end
-end
